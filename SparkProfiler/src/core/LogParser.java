afb8b9679239032f560305b6eefa5c74a3707db0
tawfiq <tawfiq@tawfiq-unimelbPC>
2016-11-24 18:20:25 +1100

    6th....done log parser
    still need to parse the whole directory of logs for all applications

--- SparkProfiler/src/core/LogParser.java
+++ SparkProfiler/src/core/LogParser.java
@@ -4,11 +4,14 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import org.json.*;
 
 public class LogParser {
 	public static void parseLog() {
 
 		BufferedReader br = null;
+		long startTime=0;
+		long endTime = 0;
 		try {
 			br = new BufferedReader(new FileReader("/home/tawfiq/sp/spark-2.0.1/logs/app-20161124151631-0003"));
 		} catch (FileNotFoundException e) {
@@ -18,9 +21,27 @@ public class LogParser {
 		String line;
 		try {
 			while ((line = br.readLine()) != null) {
-				if (line.contains("Complete Population")){
-					// do something
-					break; // breaks the while loop
+				JSONObject obj = null;
+				try {
+					obj = new JSONObject(line);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					String n = obj.getString("Event");
+					if(n.equalsIgnoreCase("SparkListenerApplicationStart"))
+					{
+					   startTime=obj.getLong("Timestamp");
+					}
+					if(n.equalsIgnoreCase("SparkListenerApplicationEnd"))
+					{
+					   endTime=obj.getLong("Timestamp");
+					}
+					//System.out.println(n);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 			}
 		} catch (IOException e) {
@@ -28,16 +49,7 @@ public class LogParser {
 			e.printStackTrace();
 		}
 
-		// we reached the section with numbers
-		try {
-			while ((line = br.readLine()) != null) {
-				// use String.split to split the line, then convert 
-				//the values to double and process them.  
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		System.out.println(endTime-startTime);
 
 	}
 
